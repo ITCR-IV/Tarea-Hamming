786e4dcac0e091b6043c4df196a4138aa9292dad
Ignacio Vargas <ignaevc@hotmail.com>
2021-03-17 20:33:44 -0600

    Arreglado y expandido layout de gui

--- Hamming.py
+++ Hamming.py
@@ -23,6 +23,7 @@ def Validar():
 
     if len(dato_recibido) == 12:
         Conversiones(dato_recibido)
+        l_informacion.config(text="")
     else:
         l_informacion.config(text="Error en el largo")
 
@@ -65,7 +66,7 @@ def Binario_a_Decimal(binario):
 """
 ventana_principal = tk.Tk()
 ventana_principal.title("DISEÑO LÓGICO")
-ventana_principal.minsize(500, 450)
+ventana_principal.minsize(600, 450)
 ventana_principal.resizable(width=tk.NO, height=tk.NO)
 
 fondo = tk.Canvas(ventana_principal, width=600, height=600, bg='DodgerBlue4')
@@ -76,16 +77,18 @@ fondo.place(x=0, y=0)
 
 # Label con el título principal
 l_titulo = tk.Label(ventana_principal, text="PROYECTO DISEÑO LÓGICO ", font=("Verdana", 14), bg="DodgerBlue4", fg="maroon1")
-l_titulo.place(x=130, y=10)
+l_titulo.place(x=160, y=10)
 
 # Label de información
-titulo = tk.Label(ventana_principal, text="Ingrese un dato binario de 12 bits", font=("Agency FB", 14), bg="DodgerBlue4", fg="black")
+titulo = tk.Label(ventana_principal, text="Ingrese un dato binario de 12 bits: ", font=("Agency FB", 14), bg="DodgerBlue4", fg="black")
 titulo.place(x=10, y=50)
+
+ventana_principal.update()  # para poder obtener el tamaño del label luego
 
 # Entry para ingresar el dato binario
 entrada_binaria = tk.StringVar()  # número que se ingresa
 ingresar_dato = tk.Entry(ventana_principal, width=15, font=("Agency", 14), bg="black", fg="yellow", textvariable=entrada_binaria)
-ingresar_dato.place(x=220, y=50)
+ingresar_dato.place(x=10+titulo.winfo_width(), y=50)
 
 
 def limitar_entrada(entrada_binaria):
@@ -100,33 +103,21 @@ entrada_binaria.trace("w", lambda *args: limitar_entrada(entrada_binaria))
 
 
 # Label para información
-l_informacion = tk.Label(ventana_principal, text="", font=("Agency FB", 16), bg="DodgerBlue4", fg="black")
-l_informacion.place(x=10, y=80)
+l_informacion = tk.Label(ventana_principal, text="", font=("Agency FB", 16), bg="DodgerBlue4", fg="red")
+l_informacion.place(x=360, y=105)
 
 # ListBox para desplegar la información
 lista = tk.Listbox(ventana_principal, bg="yellow", width=50, height=5)
-lista.place(x=10, y=120)
+lista.place(x=100, y=150)
+
+# Botón calcular
+boton_calcular = tk.Button(ventana_principal, bg='black', text="Calcular", command=Validar, fg="lime", font=("Agency FB", 14))
+boton_calcular.place(x=250, y=100)
 
 
-# ingresar_edad = Entry(ventana_principal, width=10, font=("Agency", 14), bg="white", fg="blue")
-# ingresar_edad.place(x=10, y=50)
-
-
-# Label con el texto ingresar edad
-# l_numero = Label(ventana_principal, text="Ingrese un número: ", font=("Agency FB", 14), bg="black", fg="magenta")
-# l_numero.place(x=10, y=150)
-
-
-# Botón con imagen
-# flecha = CargarImg("right-arrow.gif")
-boton_validacion = tk.Button(ventana_principal, bg='black', text="Calcular", command=Validar, fg="lime", font=("Agency FB", 14))
-# boton_ima1.config(image=flecha)
-boton_validacion.place(x=400, y=40)
-
-
-# Botón simple
+# Botón salir
 boton_ingresar1 = tk.Button(ventana_principal, text="SALIR", command=Salir, bg="DarkRed", fg="black", font=("Verdana", 12))
-boton_ingresar1.place(x=410, y=380)
+boton_ingresar1.place(x=500, y=400)
 
 
 # Fin del código de la ventana principal
